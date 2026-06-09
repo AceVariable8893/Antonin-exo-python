2c79c01b43273f33d248c09f05d11cdb2f840ea7
AceVariable <antonin.bigotte@gmail.com>
2026-06-09 14:05:03 +0200

    étapes 1-4

---
+++ 01-basics/hello.py
@@ -0,0 +1,17 @@
+print("Bonjour le monde !")
+
+
+print("Python", "c'est parti")
+print(2026)
+print(10 + 5)
+
+
+prenom = "Lucas"
+age = 25
+
+print(prenom)
+print(age)
+
+
+prenom = input("Quel est ton prénom ?")
+print(prenom)
