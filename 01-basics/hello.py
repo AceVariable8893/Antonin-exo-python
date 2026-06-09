9831a4ce988e3314fd840e3508d88083db02e964
AceVariable <antonin.bigotte@gmail.com>
2026-06-09 14:12:51 +0200

    Fin atelier 1.1

--- 01-basics/hello.py
+++ 01-basics/hello.py
@@ -1,17 +1,27 @@
+#Etape 1
 print("Bonjour le monde !")
 
-
+#Etape 2
 print("Python", "c'est parti")
 print(2026)
 print(10 + 5)
 
-
+#Etape 3
 prenom = "Lucas"
 age = 25
 
 print(prenom)
 print(age)
 
-
+#Etape 4
 prenom = input("Quel est ton prénom ?")
 print(prenom)
+
+#Etape 5
+prenom = input("Quel est ton prénom ? ")
+print(f"Bonjour {prenom}, bienvenue dans la formation Python !")
+
+#Exercice
+prenom = input("Quel est ton prénom ? ")
+age = input("Quel est ton âge ? ")
+print(f"{prenom}, dans 10 ans tu auras {int(age) + 10} ans.")
